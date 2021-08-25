7643005b018bee1b95a84925ffbfd4ab4397c8f2
nikolay66613 <79995069+nikolay66613@users.noreply.github.com>
2021-08-25 14:06:53 +0300

    Add files via upload
    Код опросника с подсветкой верных ответов.

---
+++ main.js
@@ -0,0 +1,185 @@
+const option1 = document.querySelector('.option1'),
+    option2 = document.querySelector('.option2'),
+    option3 = document.querySelector('.option3'),
+    option4 = document.querySelector('.option4');
+
+const optionElements = document.querySelectorAll('.option');
+
+const question = document.getElementById('question');
+
+const numberOfQuestin = document.getElementById('number-of-question'),
+    numberOfAllQuestions = document.getElementById('number-of-all-questions');
+
+let indexOfQuestion,
+    indexOfPage = 0;
+
+const answersTracker = document.getElementById('answers-tracker');
+const btnNext = document.getElementById('btn-next');
+
+let score = 0;
+
+const correctAnswer = document.getElementById('correct-answer'),
+    numberOfAllQuestions2 = document.getElementById('number-of-all-questions-2'),
+    btnTryAgain = document.getElementById('btn-try-again');
+
+const questions = [
+    {
+        question: 'Вопрос 1',
+        options: [
+            'ответ 1',
+            'ответ 2',
+            'ответ 3',
+            'ответ 4',
+        ],
+        rightAnswer: 2
+    },
+    {
+        question: 'Вопрос 2',
+        options: [
+            'ответ 1',
+            'ответ 2',
+            'ответ 3',
+            'ответ 4',
+        ],
+        rightAnswer: 2
+    },
+    {
+        question: 'Вопрос 3',
+        options: [
+            'ответ 1',
+            'ответ 2',
+            'ответ 3',
+            'ответ 4',
+        ],
+        rightAnswer: 2
+    },
+    {
+        question: 'Вопрос 4',
+        options: [
+            'ответ 1',
+            'ответ 2',
+            'ответ 3',
+            'ответ 4',
+        ],
+        rightAnswer: 2
+    }
+];
+
+
+numberOfAllQuestions.innerHTML = questions.length;
+
+const load = () => {
+    question.innerHTML = questions[indexOfQuestion].question;
+
+    option1.innerHTML = questions[indexOfQuestion].options[0];
+    option2.innerHTML = questions[indexOfQuestion].options[1];
+    option3.innerHTML = questions[indexOfQuestion].options[2];
+    option4.innerHTML = questions[indexOfQuestion].options[3];
+
+    numberOfQuestin.innerHTML = indexOfPage + 1;
+    indexOfPage++;
+};
+
+let completedAnswers = []
+
+const randomQuest = () =>{
+    let randomNumber = Math.floor(Math.random() * questions.length);
+    let hitDuplicate = false;
+
+    if(indexOfPage == questions.length) {
+        quizOver()
+    } else{
+        if(completedAnswers.length > 0) {
+            completedAnswers.forEach(item => {
+                if(item == randomNumber){
+                    hitDuplicate = true;
+                }
+            });
+            if(hitDuplicate) {
+                randomQuest();
+            }else {
+                indexOfQuestion = randomNumber;
+                load();
+            }
+        }
+        if(completedAnswers.length == 0) {
+            indexOfQuestion = randomNumber;
+            load();
+        }
+    }
+    completedAnswers.push(indexOfQuestion);
+};
+
+const checkAnswer = el => {
+    if(el.target.dataset.id == questions[indexOfQuestion].rightAnswer) {
+        el.target.classList.add('correct');
+        updateAnswerTracker('correct');
+        score++;
+    }else {
+        el.target.classList.add('wrong');
+        updateAnswerTracker('wrong');
+    }
+    disableOptions();
+}
+
+for(option of optionElements){
+    option.addEventListener('click', e => checkAnswer(e));
+}
+
+const disableOptions = () => {
+    optionElements.forEach(item => {
+        item.classList.add('disabled');
+        if(item.dataset.id == questions[indexOfQuestion].rightAnswer) {
+            item.classList.add('correct');
+        }
+    })
+}
+
+const anableOptions = () =>{
+    optionElements.forEach(item=> {
+        item.classList.remove('disabled', 'correct', 'wrong');
+    })
+}
+
+const answerTracker = () => {
+    questions.forEach(()=>{
+        const div = document.createElement('div');
+        answersTracker.appendChild(div);
+    })
+};
+
+
+const updateAnswerTracker = status => {
+    answersTracker.children[indexOfPage - 1].classList.add(`${status}`);
+}
+
+const validate = () => {
+    if(!optionElements[0].classList.contains('disabled')) {
+        alert('Вам нужно выбрать один из вариантов ответа');
+    }else{
+        randomQuest();
+        anableOptions();
+    }
+};
+
+
+const quizOver = () =>{
+    document.querySelector('.quiz-over-modal').classList.add('active');
+    correctAnswer.innerHTML = score;
+    numberOfAllQuestions2.innerHTML = questions.length;
+};
+
+const tryAgain = () => {
+    window.location.reload();
+};
+
+btnTryAgain.addEventListener('click', tryAgain);
+
+btnNext.addEventListener('click', () => {
+    validate();
+});
+
+window.addEventListener('load', () => {
+    randomQuest();
+    answerTracker();
+});
